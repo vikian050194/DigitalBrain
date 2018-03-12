808c897d18a40958632e6dfbd9ebb2a4322e8318
Kirill Vinogradov <vikian050194@gmail.com>
2018-03-12 10:54:00 +0300

    Fix problem with path to bundle.js;

--- client/js/index.js
+++ client/js/index.js
@@ -1,38 +1,37 @@
 $(document).ready(function () {
-    //alert("Hello, World!");
-    debugger;
     $.ajaxSetup({ cache: false });
+    $('#input').focus();
+
+    var task = undefined;
+
+    function start() {
+        $('#input').val('');
+
+        $.ajax({
+            type: "POST",
+            url: '/task',
+            success: function (tasks) {
+                task = tasks[0];
+                $('#task').html(`${task.a} ${task.operation} ${task.b} = ?`);
+                $('#input').focus();
+            }
+        });
+    }
+
+    function changeScore(isCorrect) {
+        var plus = '<span style="color:green;">+</span>';
+        var minus = '<span style="color:red;">-</span>';
+        var newContent = $('#result').html() + (isCorrect ? plus : minus);
+        $('#result').html(newContent);
+    }
 
     $("#input").on("keydown", function (e) {
         if (e.which == 13) {
-            alert('You pressed enter!');
+            var isCorrect = $("#input").val() == task.result;
+            changeScore(isCorrect);
+            start();
         }
     });
 
-    // $.ajax({
-    //     type: "GET",
-    //     url: this.href,
-    //     success: function (result) {
-    //         reload();
-    //     }
-    // });
-
-    // $.ajax({
-    //     type: "GET",
-    //     url: '/Group/List',
-    //     success: function (result) {
-    //         $('#group').html(result);
-    //     }
-    // });
-
-    // $(".refresher").on("click", function () {
-    //     console.log("click");
-    //     $.ajax({
-    //         type: "GET",
-    //         url: this.href,
-    //         success: function (result) {
-    //             reload();
-    //         }
-    //     });
-    // });
+    start();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './client/js/index.js',
     output: {
         filename: 'bundle.js',
-        path: __dirname + '/client'
+        path: __dirname + '/client/build'
     },
     plugins: [
         new webpack.ProvidePlugin({
